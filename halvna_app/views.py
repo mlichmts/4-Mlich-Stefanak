5dc12b4554d8bd55096224994b412171d22e32a2
cvicenia <mlichmatus0@gmail.com>
2026-04-22 09:57:47 +0200

    dalej

--- halvna_app/views.py
+++ halvna_app/views.py
@@ -1,22 +1,34 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .models import Task
+from .models import Task, Category
 
 @login_required
 def task_list(request):
-    tasks = Task.objects.filter(user=request.user)
-    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+    if request.user.is_superuser:
+        tasks = Task.objects.all()
+    else:
+        tasks = Task.objects.filter(user=request.user)
+
+    return render(request, 'tasks/task_list.html', {
+        'tasks': tasks})
 
 
 @login_required
 def task_create(request):
+    categories = Category.objects.all()
+
     if request.method == 'POST':
         Task.objects.create(
             title=request.POST['title'],
             description=request.POST.get('description'),
-            due_date=request.POST['due_date'],
+            due_date=request.POST.get('due_date'),
+            priority=request.POST.get('priority'),
+            status=request.POST.get('status'),
+            category_id=request.POST.get('category'),
             user=request.user
         )
         return redirect('task_list')
 
-    return render(request, 'tasks/task_form.html')
+    return render(request, 'tasks/task_form.html', {
+        'categories': categories
+    })
